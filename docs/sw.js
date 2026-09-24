379ad41ecea6f207cd0814e8652b2426caa4e837
Cuto1 <109393704+Cuto1@users.noreply.github.com>
2026-09-24 15:52:19 -0300

    Atualiza cache para correção do topo web

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='diagnostico-er-web-v16-responsive-hero';
+const CACHE='diagnostico-er-web-v17-hero-crop-fix';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
