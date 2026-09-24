a12d0661276138047d36cd9efeca3c57a6f48c65
Cuto1 <109393704+Cuto1@users.noreply.github.com>
2026-09-24 14:39:18 -0300

    Atualiza cache para editor visual completo

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='diagnostico-er-web-v11-branding-editor';
+const CACHE='diagnostico-er-web-v12-full-branding';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
