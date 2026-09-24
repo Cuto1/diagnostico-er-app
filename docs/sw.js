31be4554df8ce90a79d745d00956b0f415eb40b1
Cuto1 <109393704+Cuto1@users.noreply.github.com>
2026-09-24 14:10:08 -0300

    Atualiza cache para área de Materiais

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='diagnostico-er-web-v9-chat-alerts';
+const CACHE='diagnostico-er-web-v10-materials';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
