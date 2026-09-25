65da36cf2c0cb54fc0a5c8c613798f1340988599
Cuto1 <109393704+Cuto1@users.noreply.github.com>
2026-09-25 10:35:43 -0300

    Atualiza cache para nova tipografia

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='diagnostico-er-web-v25-font-bump';
+const CACHE='diagnostico-er-web-v26-font-bump-2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
