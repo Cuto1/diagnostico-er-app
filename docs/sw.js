53cd55a905a3cc8d0b945c9a5755c8a0d0f0f0c5
Cuto1 <109393704+Cuto1@users.noreply.github.com>
2026-09-25 11:20:25 -0300

    Atualiza cache para presença online confiável

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='diagnostico-er-web-v33-duel-ranking';
+const CACHE='diagnostico-er-web-v34-online-heartbeat';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
