62ffb487c9a6279e46e5335ac4a6a774c24f2401
Cuto1 <109393704+Cuto1@users.noreply.github.com>
2026-09-25 14:41:32 -0300

    Atualiza cache do ranking acumulado

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='diagnostico-er-web-v37-ranking-detalhado';
+const CACHE='diagnostico-er-web-v38-ranking-acumulado';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
