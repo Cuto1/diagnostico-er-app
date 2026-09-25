de57193b9f57b1da74868c9aa78f3d2620c9ece0
Cuto1 <109393704+Cuto1@users.noreply.github.com>
2026-09-25 14:32:19 -0300

    Atualiza cache para ranking detalhado

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='diagnostico-er-web-v36-live-quiz-3plus';
+const CACHE='diagnostico-er-web-v37-ranking-detalhado';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
